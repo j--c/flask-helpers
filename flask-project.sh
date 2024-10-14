2142420ac77ac1e5341317c551900c1a79e8991c
j--c <johnmichaelcollins@protonmail.com>
2024-10-14 12:37:24 -0400

    updated the script to create some basic files too

--- flask-project.sh
+++ flask-project.sh
@@ -18,6 +18,41 @@ function write_gitignore(){
     echo "venv/" >> .gitignore
 }
 
+function create_project_file(){
+    file_path="$project_name.py"
+    touch $file_path
+    echo "from app import create_app" >> $file_path 
+    echo "" >> $file_path
+    echo "" >> $file_path
+    echo "app = create_app()" >> $file_path
+}
+
+function create_config_file(){
+    file_path="config.py"
+    touch $file_path
+    echo "class Config:" >> $file_path 
+    echo "    TEST_VALUE = '123'" >> $file_path
+}
+
+function create_app_init_file(){
+    app_dir_name="app"
+    app_init_file_name="__init__.py"
+    file_path="$app_dir_name/$app_init_file_name"
+    mkdir $app_dir_name
+    touch $file_path
+    echo "from flask import Flask" >> $file_path
+    echo "from config import Config" >> $file_path
+    echo "" >> $file_path
+    echo "def create_app(config_class=Config):" >> $file_path
+    echo "    app = Flask(__name__)" >> $file_path
+    echo "    app.config.from_object(config_class)" >> $file_path
+}
+
+function create_basic_files(){
+    create_project_file
+    create_config_file
+    create_app_init_file
+}
 
 if [ -z "$project_name" ]; then
     echo "Project name required!"
@@ -38,4 +73,7 @@ else
 
     echo "Creating git repo..."
     git init -b main
+
+    echo "Creating basic files..."
+    create_basic_files
 fi
